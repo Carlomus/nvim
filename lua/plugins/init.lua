39cdae0a6a388bdf38d5e3d5bf940172958200db
Carlo <Carlomus@gmail.com>
2025-05-01 20:09:05 +0100

    added molten

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -6,6 +6,32 @@ return {
 		ft = { "python", "ipynb" },
 	},
 	{
+		"3rd/image.nvim",
+		build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+		opts = {
+			processor = "magick_cli",
+			backend = "kitty", -- Kitty will provide the best experience, but you need a compatible terminal
+			integrations = {}, -- do whatever you want with image.nvim's integrations
+			max_width = 100, -- tweak to preference
+			max_height = 12, -- ^
+			max_height_window_percentage = math.huge, -- this is necessary for a good experience
+			max_width_window_percentage = math.huge,
+			window_overlap_clear_enabled = true,
+			window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
+		},
+	},
+	{
+		"benlubas/molten-nvim",
+		version = "^1.0.0", -- use version <2.0.0 to avoid breaking changes
+		dependencies = { "3rd/image.nvim" },
+		build = ":UpdateRemotePlugins",
+		init = function()
+			-- these are examples, not defaults. Please see the readme
+			vim.g.molten_image_provider = "image.nvim"
+			vim.g.molten_output_win_max_height = 20
+		end,
+	},
+	{
 		"nvchad/base46",
 		build = function()
 			require("base46").load_all_highlights()

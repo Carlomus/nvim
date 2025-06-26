5a47659eefd378cabd508281221c8750bd54e246
Carlo <Carlomus@gmail.com>
2025-06-26 17:13:31 +0100

    removed session restore - was annowing

--- lua/plugins/qol.lua
+++ lua/plugins/qol.lua
@@ -46,22 +46,24 @@ M = {
 			require("cmp").event:on("confirm_done", cmp_autopairs.on_confirm_done())
 		end,
 	},
-	{
-		"rmagatti/auto-session",
-		lazy = false,
-
-		---enables autocomplete for opts
-		---@module "auto-session"
-		---@type AutoSession.Config
-		opts = {
-			suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
-			-- log_level = 'debug',
-		},
-	},
+	-- {
+	-- 	"rmagatti/auto-session",
+	-- 	lazy = false,
+	--
+	-- 	---enables autocomplete for opts
+	-- 	---@module "auto-session"
+	-- 	---@type AutoSession.Config
+	-- 	opts = {
+	-- 		suppressed_dirs = { "~/", "~/Projects", "~/Downloads", "/" },
+	-- 		-- log_level = 'debug',
+	-- 	},
+	-- },
 	{
 		"kylechui/nvim-surround",
+		version = "^3.0.0",
+		event = "VeryLazy",
 		config = function()
-			require("nvim-surround").setup()
+			require("nvim-surround").setup({})
 		end,
 	},
 	-- {

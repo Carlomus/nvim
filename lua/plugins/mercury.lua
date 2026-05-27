12d5c82d41cf2eae9b1252a1c939b914373ddc35
Carlo Mussolini <carlomus@gmail.com>
2026-05-27 11:03:22 +0100

    mercury: remove dev override comment block

--- lua/plugins/mercury.lua
+++ lua/plugins/mercury.lua
@@ -1,9 +1,5 @@
--- To revert to a local dev override (load from
--- /Users/carlo/no_work/mercury directly without going through
--- GitHub), uncomment the `dir = ...` line below.
 return {
 	"Carlomus/mercury",
-	-- dir = "/Users/carlo/no_work/mercury",
 	lazy = false,
 	dependencies = {
 		"3rd/image.nvim",
